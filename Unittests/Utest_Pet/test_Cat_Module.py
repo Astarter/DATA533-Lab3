8eaadbc3d7dea21fc4b88701a9eaad0a914fcfb2
Vicens <53487686+VicensPaneque@users.noreply.github.com>
2020-12-09 11:49:16 -0600

    Update test_Cat_Module.py

--- Unittests/Utest_Pet/test_Cat_Module.py
+++ Unittests/Utest_Pet/test_Cat_Module.py
@@ -1,55 +1,59 @@
 from Animals.pet_animals.cat import *
 import unittest
 
-
 class test_Cat(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        print("setUpClass")
+        print("Beginning of test class")
 
     def setUp(self):
-        self.pet1 = Cat("Garfield", "orange")
-        self.pet2 = Cat("Colonel Meow", "black")
-        print("setUp")
-
-    def test_sound(self):
-        self.assertEqual(self.pet1.sound(), "Meow meow prrrrrr")
-        self.assertEqual(self.pet2.sound(), "Meow meow prrrrrr")
-        print("test_sound")
-
-    def test_describe(self):
-        self.assertEqual(self.pet1.describe("lazy"), "Garfield, the lazy, orange cat")
-        self.assertEqual(self.pet2.describe("majestic"), "Colonel Meow, the majestic, black cat")
-        print("test_describe")
-
-    def test_feed(self):
-        self.pet1.feed(30),
-        self.assertEqual(self.pet1.weight, 6.0+30*0.1)
-        self.pet2.feed(61)
-        self.assertEqual(self.pet2.weight, 6.0+61*0.1)
-        print("test_feed")
-
-    def test_setWeight(self):
-        self.pet1.setWeight(6)
-        self.pet2.setWeight(3)
-        self.assertEqual(self.pet1.weight, 6)
-        self.assertEqual(self.pet2.weight, 3)
-
-    def test_getWeight(self):
-        self.assertEqual(self.pet1.getWeight(), "Garfield weights 6.0 kg.")
-        self.assertEqual(self.pet2.getWeight(), "Colonel Meow weights 6.0 kg.")
-
-    def test_on_a_diet(self):
-        self.pet1.on_a_diet(2)
-        self.assertEqual(self.pet1.weight, 4)
-
-        self.pet2.on_a_diet(3)
-        self.assertEqual(self.pet2.weight, 3)
-        print("test_feed")
-
+        self.cat1 = Cat("Garfield", "orange")
+        self.cat2 = Cat("Colonel Meow", "black")
+        
     def tearDown(self):
-        print("tearDown")
-
+        print("End of test case")
+        
     @classmethod
     def tearDownClass(cls):
-        print("tearDownClass")
+        print("End of test class")
+
+    def test_sound(self):
+        self.assertEqual(self.cat1.sound(), None)
+        self.assertEqual(self.cat2.sound(), None)
+        with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            self.cat1.sound()
+            self.cat2.sound()
+        assert fake_stdout.getvalue() == "Meow meow prrrrrr\n" \
+                                         "Meow meow prrrrrr\n"
+
+
+    def test_describe(self):
+        self.assertEqual(self.cat1.describe("lazy"), None)
+        self.assertEqual(self.cat2.describe("majestic"), None)
+        with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            self.cat1.describe("lazy")
+            self.cat2.describe("majestic")
+        assert fake_stdout.getvalue() == "Garfield, the lazy, orange cat\n" \
+                                         "Colonel Meow, the majestic, black cat\n"
+
+    def test_feed(self):
+        self.cat1.feed(30),
+        self.assertEqual(self.cat1.weight, 6.0+30*0.1)
+        self.cat2.feed(61)
+        self.assertEqual(self.cat2.weight, 6.0+61*0.1)
+
+    def test_setWeight(self):
+        self.cat1.setWeight(6)
+        self.cat2.setWeight(3)
+        self.assertEqual(self.cat1.weight, 6)
+        self.assertEqual(self.cat2.weight, 3)
+
+    def test_getWeight(self):
+        self.assertEqual(self.cat1.getWeight(), "Garfield weights 6.0 kg.")
+        self.assertEqual(self.cat2.getWeight(), "Colonel Meow weights 6.0 kg.")
+
+    def test_on_a_diet(self):
+        self.cat1.on_a_diet(2)
+        self.assertEqual(self.cat1.weight, 4)
+        self.catt2.on_a_diet(3)
+        self.assertEqual(self.cat2.weight, 3)
